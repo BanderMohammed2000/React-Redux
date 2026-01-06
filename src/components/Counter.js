3743c78e686372a650c8901467cc552e32921c08
Bander Btati <bander.mohammed.2000@gmail.com>
2026-01-06 20:47:33 +0300

    using redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,23 +7,32 @@ const Counter = () => {
 
   // اذا تم تحديث قيمة state.counter سيتم تحديث المكون هذا بشكل كامل
   const counter = useSelector((state) => state.counter);
+  // اذا تم تحديث قيمة state.show سيتم تحديث المكون هذا بشكل كامل
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
+  };
+
+  const increaseHandler = (value) => {
+    dispatch({ type: "increase", amount: 10 });
   };
 
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
